34795aca521123b9682e0af16df301641b824835
Daniel Rosales Alanis <20243ds001@utez.edu.mx~>
2025-09-02 11:45:40 -0600

    Daniel

---
+++ src/main/java/mx/edu/utez/Calculadora.java
@@ -0,0 +1,10 @@
+package mx.edu.utez;
+
+public class Calculadora {
+    // Atributos privados
+    private double numero1;
+    private double numero2;
+
+    // Atributo protegido
+    protected String ultimaOperacion;
+}
